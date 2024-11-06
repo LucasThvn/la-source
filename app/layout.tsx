017256bc40c24d0edd7862799e1e6d547ab65bd6
Lucas Thauvin <lucas@lazyloadatelier.com>
2024-11-06 22:44:12 +0100

    lala

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,42 +31,7 @@ export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
       <body>
-        <div className="container">
-          <header>
-            <div className="header-content">
-              <Link href="/" className="logo">
-                <img src="/logo.png" />
-              </Link>
-              <h1>
-                <span className="light">Stripe Sample</span>
-                <br />
-                Next.js, TypeScript, and Stripe 🔒💸
-              </h1>
-            </div>
-          </header>
           {children}
-        </div>
-        <div className="banner">
-          <span>
-            This is a{" "}
-            <a
-              href="https://github.com/stripe-samples"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Stripe Sample
-            </a>
-            .{" View code on "}
-            <a
-              href="https://github.com/vercel/next.js/tree/canary/examples/with-stripe-typescript"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub
-            </a>
-            .
-          </span>
-        </div>
       </body>
     </html>
   );
